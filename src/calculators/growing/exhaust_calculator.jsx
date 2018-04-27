c964799368045c6f066b88106585f2d6cc2a5c0b
jkingsman <jack.kingsman@gmail.com>
2018-04-27 14:38:20 -0700

    Add untracked files

---
+++ src/calculators/growing/exhaust_calculator.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import {GenericInput, FixedUnitInput, FixedUnitOutput} from 'app/calculators/components/io';
+import ConversionFactors from 'app/utils/conversion_factors';
+import {defaultRound} from 'app/utils/math';
+
+export default class ExhaustCalculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            growSpaceWidthFt: 10,
+            growSpaceLengthFt: 10,
+            growSpaceHeightFt: 8,
+            fanCount: 2,
+            exchangeTime: 3
+        }
+
+        this.setWidth = this.setWidth.bind(this);
+        this.setLength = this.setLength.bind(this);
+        this.setHeight = this.setHeight.bind(this);
+        this.setFanCount = this.setFanCount.bind(this);
+        this.setExchangeTime = this.setExchangeTime.bind(this);
+    }
+
+    setWidth(growSpaceWidthFt) {
+        this.setState({growSpaceWidthFt: Number(growSpaceWidthFt)});
+    }
+
+    setLength(growSpaceLengthFt) {
+        this.setState({growSpaceLengthFt: Number(growSpaceLengthFt)});
+    }
+
+    setHeight(growSpaceHeightFt) {
+        this.setState({growSpaceHeightFt: Number(growSpaceHeightFt)});
+    }
+
+    setFanCount(fanCount) {
+        this.setState({fanCount: Number(fanCount)});
+    }
+
+    setExchangeTime(exchangeTime) {
+        this.setState({exchangeTime: Number(exchangeTime)});
+    }
+
+    getRoomVolume() {
+        return (this.state.growSpaceWidthFt * this.state.growSpaceLengthFt * this.state.growSpaceHeightFt);
+    }
+
+    getFlowRate() {
+        return this.getRoomVolume() / this.state.exchangeTime;
+    }
+
+    getSingleFanFlow() {
+        return this.getFlowRate() / this.state.fanCount;
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Note that this is for pure airflow; this does not account for pressure lost due to bends in ducting, dust, etc. which is highly duct- and layout-specific.</p>
+                <div className="row">
+                    <div className="col-sm">
+                        <GenericInput inputLabel={'Room Width'} onChange={this.setWidth} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceWidthFt}/>
+                        <GenericInput inputLabel={'Room Length'} onChange={this.setLength} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceLengthFt}/>
+                        <GenericInput inputLabel={'Room Height'} onChange={this.setHeight} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceHeightFt}/>
+                        <FixedUnitInput inputLabel={'Fan Count'} onChange={this.setFanCount} number={this.state.fanCount} unit="fans"/>
+                        <FixedUnitInput inputLabel={'Exchange Time'} onChange={this.setExchangeTime} number={this.state.exchangeTime} unit="minutes to total air flush"/>
+                    </div>
+                    <div className="col-sm">
+                        <FixedUnitOutput outputLabel="Room Volume" number={defaultRound(this.getRoomVolume())} unit="ft³"/>
+                        <FixedUnitOutput outputLabel="Min. Total Flow" number={defaultRound(this.getFlowRate())} unit="CFM (ft³/min)"/>
+                        <FixedUnitOutput outputLabel="Min. Fan Flow Each" number={defaultRound(this.getSingleFanFlow())} unit="CFM (ft³/min)"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/calculators/growing/plantcount_calculator.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import {GenericInput, FixedUnitOutput} from 'app/calculators/components/io';
+import ConversionFactors from 'app/utils/conversion_factors';
+
+export default class PlantCount extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            growSpaceWidthFt: 20,
+            growSpaceLengthFt: 10,
+            plantWidthFt: 2.1,
+            plantLengthFt: 2.1,
+        }
+
+        this.setWidth = this.setWidth.bind(this);
+        this.setLength = this.setLength.bind(this);
+        this.setplantWidthFt = this.setplantWidthFt.bind(this);
+        this.setplantLengthFt = this.setplantLengthFt.bind(this);
+    }
+
+    setWidth(growSpaceWidthFt) {
+        this.setState({growSpaceWidthFt: Number(growSpaceWidthFt)});
+    }
+
+    setLength(growSpaceLengthFt) {
+        this.setState({growSpaceLengthFt: Number(growSpaceLengthFt)});
+    }
+
+    setplantWidthFt(plantWidthFt) {
+        this.setState({plantWidthFt: Number(plantWidthFt)});
+    }
+
+    setplantLengthFt(plantLengthFt) {
+        this.setState({plantLengthFt: Number(plantLengthFt)});
+    }
+
+    getPlantsPerWidth() {
+        return Math.floor(this.state.growSpaceWidthFt / this.state.plantWidthFt);
+    }
+
+    getPlantsPerLength() {
+        return Math.floor(this.state.growSpaceLengthFt / this.state.plantLengthFt);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-sm">
+                        <GenericInput inputLabel={'Grow Space Width'} onChange={this.setWidth} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceWidthFt}/>
+                        <GenericInput inputLabel={'Grow Space Length'} onChange={this.setLength} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceLengthFt}/>
+                        <GenericInput inputLabel={'Plant Length*'} onChange={this.setplantWidthFt} conversionFactors={ConversionFactors.basicDistance} number={this.state.plantWidthFt}/>
+                        <GenericInput inputLabel={'Plant Width*'} onChange={this.setplantLengthFt} conversionFactors={ConversionFactors.basicDistance} number={this.state.plantLengthFt}/>
+                    </div>
+                    <div className="col-sm">
+                        <FixedUnitOutput outputLabel="Total Area" number={this.state.growSpaceLengthFt * this.state.growSpaceWidthFt} unit="ft²"/>
+                        <FixedUnitOutput outputLabel="Total Plants" number={this.getPlantsPerWidth() * this.getPlantsPerLength()} unit="plants"/>
+                        <FixedUnitOutput outputLabel="Grid Layout" number={this.getPlantsPerWidth()} unit="plants wide"/>
+                        <FixedUnitOutput noColon number={this.getPlantsPerLength()} unit="plants long"/>
+                    </div>
+                </div>
+                <i>*Remember to add the appropriate spacing between plants, which is counted as part of its size</i>
+            </div>
+        );
+    }
+}
