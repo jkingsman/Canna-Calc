2581c71793640d8d691bf633e0e62f71017ef765
jkingsman <jack.kingsman@gmail.com>
2018-04-29 17:37:19 -0700

    Optimize layout calculator

--- src/calculators/growing/plantcount_calculator.jsx
+++ src/calculators/growing/plantcount_calculator.jsx
@@ -7,10 +7,10 @@ export default class PlantCount extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            growSpaceWidthFt: 20,
-            growSpaceLengthFt: 10,
-            plantWidthFt: 2.1,
-            plantLengthFt: 2.1,
+            growSpaceWidthFt: 17,
+            growSpaceLengthFt: 8,
+            plantWidthFt: 3,
+            plantLengthFt: 2,
         }
 
         this.setWidth = this.setWidth.bind(this);
@@ -35,29 +35,44 @@ export default class PlantCount extends React.Component {
         this.setState({plantLengthFt: Number(plantLengthFt)});
     }
 
-    getPlantsPerWidth() {
-        return Math.floor(this.state.growSpaceWidthFt / this.state.plantWidthFt);
+    getPlantLayout() {
+        const stdWCount = Math.floor(this.state.growSpaceWidthFt / this.state.plantWidthFt);
+        const stdLCount = Math.floor(this.state.growSpaceLengthFt / this.state.plantLengthFt);
+
+        const swapWCount = Math.floor(this.state.growSpaceWidthFt / this.state.plantLengthFt);
+        const swapLCount = Math.floor(this.state.growSpaceLengthFt / this.state.plantWidthFt);
+
+        if (stdWCount * stdLCount > swapWCount * swapLCount) {
+            return [stdWCount, stdLCount];
+        } else {
+            return [swapLCount, swapWCount];
+        }
     }
 
-    getPlantsPerLength() {
-        return Math.floor(this.state.growSpaceLengthFt / this.state.plantLengthFt);
+    getWastedSpace() {
+        const usedSpace = (this.getPlantLayout()[0] * this.getPlantLayout()[1]) * (this.state.plantWidthFt * this.state.plantLengthFt);
+        const totalSpace = this.state.growSpaceLengthFt * this.state.growSpaceWidthFt;
+        return totalSpace - usedSpace;
     }
 
     render() {
         return (
             <div>
+                <p>This calculator works best with square or near-square plants. For non-square plants, it may be possible to place additional plants in the leftover space.</p>
+                <hr/>
                 <div className="row">
                     <div className="col-sm">
                         <GenericInput inputLabel="Grow Space Width" onChange={this.setWidth} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceWidthFt}/>
                         <GenericInput inputLabel="Grow Space Length" onChange={this.setLength} conversionFactors={ConversionFactors.basicDistance} number={this.state.growSpaceLengthFt}/>
-                        <GenericInput inputLabel="Plant Length*" onChange={this.setplantWidthFt} conversionFactors={ConversionFactors.basicDistance} number={this.state.plantWidthFt}/>
                         <GenericInput inputLabel="Plant Width*" onChange={this.setplantLengthFt} conversionFactors={ConversionFactors.basicDistance} number={this.state.plantLengthFt}/>
+                        <GenericInput inputLabel="Plant Length*" onChange={this.setplantWidthFt} conversionFactors={ConversionFactors.basicDistance} number={this.state.plantWidthFt}/>
                     </div>
                     <div className="col-sm">
                         <FixedUnitOutput outputLabel="Total Area" number={this.state.growSpaceLengthFt * this.state.growSpaceWidthFt} unit="ft²"/>
-                        <FixedUnitOutput outputLabel="Total Plants" number={this.getPlantsPerWidth() * this.getPlantsPerLength()} unit="plants"/>
-                        <FixedUnitOutput outputLabel="Grid Layout" number={this.getPlantsPerWidth()} unit="plants wide"/>
-                        <FixedUnitOutput noColon number={this.getPlantsPerLength()} unit="plants long"/>
+                        <FixedUnitOutput outputLabel="Total Plants" number={this.getPlantLayout()[0] * this.getPlantLayout()[1]} unit="plants"/>
+                        <FixedUnitOutput outputLabel="Grid Layout" number={this.getPlantLayout()[0]} unit="plants wide by"/>
+                        <FixedUnitOutput noColon number={this.getPlantLayout()[1]} unit="plants long"/>
+                        <FixedUnitOutput outputLabel="Wasted Space" number={this.getWastedSpace()} unit="ft²"/>
                     </div>
                 </div>
                 <i>*Remember to add the appropriate spacing between plants, which is counted as part of its size</i>
