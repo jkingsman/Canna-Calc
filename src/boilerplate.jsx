1deb76221a652134e6b0816f3c8b4647d877bc44
jkingsman <jack.kingsman@gmail.com>
2018-05-02 10:40:41 -0700

    Add license agreement

--- src/boilerplate.jsx
+++ src/boilerplate.jsx
@@ -3,21 +3,24 @@ import CalculatorContainer from 'app/calculators/calculator_container';
 
 export default class Boilerplate extends React.Component {
     render() {
-        return (
-            <div>
-                <div className="container">
-                    <div className="alert alert-success" role="alert">
-                        <h4 className="text-center">Canna-Calc has the calculators you need for growing, concentrates, yield, and more! Click the header to open a calculator.</h4>
-                    </div>
-                    <CalculatorContainer/>
+        return (<div>
+            <div className="container">
+                <div className="alert alert-success" role="alert">
+                    <h4 className="text-center">Canna-Calc has the calculators you need for growing, concentrates, yield, and more! Click the header to open a calculator.</h4>
                 </div>
-
-                <footer className="footer">
-                    <div className="container">
-                        <span className="text-muted">This project is open source and not for profit! Contribute, comment, or ask questions at <a rel="noopener noreferrer" target="_blank" href="https://github.com/jkingsman/Canna-Calc">Github</a>.</span>
-                    </div>
-                </footer>
+                <CalculatorContainer/>
             </div>
-        );
+
+            <footer className="footer">
+                <div className="container">
+                    <span className="text-muted">This project is open source and not for profit! Contribute, comment, or ask questions at{' '}
+                        <a rel="noopener noreferrer" target="_blank" href="https://github.com/jkingsman/Canna-Calc">Github</a>.</span>
+                    <br/>
+                    <small className="text-muted">Usage of this service constitutes agreement to the{' '}
+                        <a rel="noopener noreferrer" target="_blank" href="https://opensource.org/licenses/MIT">MIT License</a>, which this work is licensed under.
+                    </small>
+                </div>
+            </footer>
+        </div>);
     }
 }
