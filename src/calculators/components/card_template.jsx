2131a831ca2517254cf4ec10d01016b6da7da89c
jkingsman <jack.kingsman@gmail.com>
2018-05-01 17:08:34 -0700

    Anchors and componentification of card

--- src/calculators/components/card_template.jsx
+++ src/calculators/components/card_template.jsx
@@ -5,48 +5,53 @@ import debounce from 'lodash/debounce'
 
 import newId from 'app/utils/unique_key';
 
-const CardTemplate = ({
-    id,
-    title,
-    children,
-    parentID,
-    keywords,
-    searchTerm
-}) => {
-    const toggleHash = () => {
-        if (window.location.hash.length < 1 || window.location.hash != `#${id}`) {
-            window.location.hash = id;
+export default class CardTemplate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.uniqueID = newId();
+
+        this.toggleHash = this.toggleHash.bind(this);
+        this.domRef = React.createRef()
+    }
+
+    toggleHash() {
+        if (window.location.hash.length < 1 || window.location.hash != `#${this.props.id}`) {
+            window.location.hash = this.props.id;
         } else {
             history.pushState("", document.title, window.location.pathname + window.location.search);
         }
     }
 
-    const shouldShow = () => (window.location.hash == `#${id}`)
-
-    const uniqueID = newId();
-
-    const matchesKeyword = searchTerm.trim().split(" ").every(singleTerm => keywords.includes(singleTerm.toLowerCase()));
-
-    if (matchesKeyword) {
-        return (
-            <div className="card pad-left">
-                <h6 className="card-header mb-0" onClick={debounce(toggleHash, 250)} id={`card${uniqueID}`} data-target={`#cardCollapse${uniqueID}`} aria-controls={`cardCollapse${uniqueID}`} data-toggle="collapse" aria-expanded="false">
-                    {title}
-                </h6>
-
-                <div id={"cardCollapse" + uniqueID} className={shouldShow()
-                    ? "collapse show"
-                    : "collapse"} aria-labelledby={"card" + uniqueID} data-parent={`#${parentID}`}>
-                    <div className="card-body">
-                        {children}
-
-                    </div>
-                </div>
-            </div>
-        );
+    shouldShow() {
+        return window.location.hash == `#${this.props.id}`;
     }
 
-    return null;
+    matchesKeyword() {
+        return this.props.searchTerm.trim().split(" ").every(singleTerm => this.props.keywords.includes(singleTerm.toLowerCase()));
+    }
+
+    render() {
+        if (this.matchesKeyword()) {
+            return (
+                <div className="card pad-left" ref={this.domRef}>
+                    <a name={this.props.id} />
+                    <h6 className="card-header mb-0" onClick={debounce(this.toggleHash, 250)} id={`card${this.uniqueID}`} data-target={`#cardCollapse${this.uniqueID}`} aria-controls={`cardCollapse${this.uniqueID}`} data-toggle="collapse" aria-expanded="false">
+                        {this.props.title}
+                    </h6>
+
+                    <div id={"cardCollapse" + this.uniqueID} className={this.shouldShow()
+                        ? "collapse show"
+                        : "collapse"} aria-labelledby={"card" + this.uniqueID} data-parent={`#${this.props.parentID}`}>
+                        <div className="card-body">
+                            {this.props.children}
+
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return null;
+    }
 }
 
 CardTemplate.propTypes = {
@@ -58,7 +63,5 @@ CardTemplate.propTypes = {
         PropTypes.arrayOf(PropTypes.element)
     ]),
     keywords: PropTypes.string,
-    searchTerm: PropTypes.string,
+    searchTerm: PropTypes.string
 };
-
-export default CardTemplate;
