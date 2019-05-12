60cc913d6b66fbde308b3fad1520ef7c1cc293a8
jkingsman <jack.kingsman@gmail.com>
2019-05-11 22:38:10 -0400

    Some tidy and logic rework

--- src/calculators/plants_consumables/plantcount_calculator.jsx
+++ src/calculators/plants_consumables/plantcount_calculator.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { GenericInput, FixedUnitOutput } from "app/calculators/components/io";
+import { round } from "app/utils/math";
 import ConversionFactors from "app/utils/conversion_factors";
 
 export default class PlantCount extends React.Component {
@@ -33,6 +34,8 @@ export default class PlantCount extends React.Component {
          * I wrote it jetlagged and hungover BUT hopefully
          * some liberal commenting will help you make some
          * sense of this.
+         *
+         * inb4 "someone already wrote a paper on this"
          */
 
          /* To start with, we do a naive block layout. 'std'
@@ -41,88 +44,113 @@ export default class PlantCount extends React.Component {
           * That relationship is inverted for 'swap'.
           * Basically, tile the plants one way, then the
           * other way.
-          */
-        const stdWCount = Math.floor(this.state.growSpaceWidthFt / this.state.plantWidthFt);
-        const stdLCount = Math.floor(this.state.growSpaceLengthFt / this.state.plantLengthFt);
-
-        const swapWCount = Math.floor(this.state.growSpaceWidthFt / this.state.plantLengthFt);
-        const swapLCount = Math.floor(this.state.growSpaceLengthFt / this.state.plantWidthFt);
-
-        let results = {
-            plantsLong: 0,
-            plantsWide: 0,
-            wastedSpacePlants: 0,
+          *
+          * After the naive blocking, we'll see if there's
+          * room to work in more plants around the edges,
+          * which I'll call "crackfilling"
+          *
+          * We'll compile the results of the std + crackfilling
+         * and swap + crackfilling, then pick the best at the end.
+         */
+         
+        // compile std
+        const resultsStd = {
+            plantsLong: Math.floor(this.state.growSpaceLengthFt / this.state.plantLengthFt),
+            plantsWide: Math.floor(this.state.growSpaceWidthFt / this.state.plantWidthFt),
         };
 
-        /* If we can't permute either way (e.g. dimensions
-         * are too small),. return the default value set
-         * (zero everything)
+        /* After our naive layout, we have two cases for
+         * possible crackfilling layout -- down one edge,
+         * or down the other.
+         *
+         * We do a simple "how many can we fit" check
+         * on both these options, then take the max.
          */
-        if (stdWCount * stdLCount == 0 && swapWCount * swapLCount == 0) {
-            return results;
+        const crackFillCandidateStdWide = this.simpleMaxLayout(
+            // Hint: The edge space's dimensions will be the leftover space * how long that edge is
+            //       Here, we check the "wide" leftover space.
+            (this.state.growSpaceWidthFt - (this.state.plantWidthFt * resultsStd.plantsWide)),
+            this.state.growSpaceLengthFt,
+            this.state.plantWidthFt,
+            this.state.plantLengthFt
+        );
+
+        const crackFillCandidateStdLong = this.simpleMaxLayout(
+            // ...and here the "long" leftover space
+            this.state.growSpaceWidthFt,
+            (this.state.growSpaceLengthFt - (this.state.plantLengthFt * resultsStd.plantsLong )),
+            this.state.plantWidthFt,
+            this.state.plantLengthFt
+        );
+
+        /* I'm fairly certain one of these will always be zero; obv.
+         * a non-zero result for both means we can fill both cracks
+         * and that changes the whole game. HOWEVER, my mental/
+         * back of the napkin proof (too drunken to post here)
+         * and also some brute force testing would seem to 
+         * indicate that isn't possible.
+         * 
+         * Prove me wrong and I'll buy you a beer.
+         */
+        resultsStd.crackFill = Math.max(crackFillCandidateStdWide, crackFillCandidateStdLong);
+
+        // Same as above, but for the "swap" naive packing solution + crackfilling
+        const resultsSwap = {
+            plantsLong: Math.floor(this.state.growSpaceLengthFt / this.state.plantWidthFt),
+            plantsWide: Math.floor(this.state.growSpaceWidthFt / this.state.plantLengthFt),
+        };
+
+        const crackFillCandidateSwapWide = this.simpleMaxLayout(
+            (this.state.growSpaceWidthFt - (this.state.plantLengthFt * resultsSwap.plantsWide)),
+            this.state.growSpaceLengthFt,
+            this.state.plantWidthFt,
+            this.state.plantLengthFt
+        );
+
+        const crackFillCandidateSwapLong = this.simpleMaxLayout(
+            this.state.growSpaceWidthFt,
+            (this.state.growSpaceLengthFt - (this.state.plantWidthFt * resultsSwap.plantsLong )),
+            this.state.plantWidthFt,
+            this.state.plantLengthFt
+        );
+
+        resultsSwap.crackFill = Math.max(crackFillCandidateSwapWide, crackFillCandidateSwapLong);
+
+        /* We've compiled our std and swap orientation
+         * along with their respective optimal crackfill
+         * scenarios; time to prep for returning data.
+         */
+
+        // idiot filter; if there's no room return all zero
+        if (resultsStd.plantsLong * resultsStd.plantsWide == 0 &&
+            resultsSwap.plantsLong * resultsSwap.plantsWide == 0){
+            return {
+                plantsLong: 0,
+                plantsWide: 0,
+                crackFill: 0,
+            };
         }
 
-        /* Two big cases: out std layout was the superior
-         * naive layout, or the swap was.
-         */
-        if (stdWCount * stdLCount > swapWCount * swapLCount) {
-            results.plantsLong = stdLCount;
-            results.plantsWide = stdWCount;
-
-            /* After our naive layout, we have two cases for
-             * possible "fill-in-the-gaps" layout -- down
-             * one edge, or down the other.
-             *
-             * We do a simple "how many can we fit" check
-             * on both these options, then take the max.
-             */
-
-            let leftoverCandidate1 = this.simpleMaxLayout(
-                // Hint: The edge space's dimensions will be the leftover space * how long that edge is
-                //       Here, we check the "wide" leftover space.
-                (this.state.growSpaceWidthFt - (this.state.plantWidthFt * results.plantsWide)),
-                this.state.growSpaceLengthFt,
-                this.state.plantWidthFt,
-                this.state.plantLengthFt
-            );
-
-            let leftoverCandidate2 = this.simpleMaxLayout(
-                // ...and here the "long" leftover space
-                this.state.growSpaceWidthFt,
-                (this.state.growSpaceLengthFt - (this.state.plantLengthFt * results.plantsLong )),
-                this.state.plantWidthFt,
-                this.state.plantLengthFt
-            );
-
-            results.wastedSpacePlants = Math.max(leftoverCandidate1, leftoverCandidate2)
+        // now choose the optimal result
+        if ((resultsStd.plantsLong * resultsStd.plantsWide + resultsStd.crackFill) > 
+           (resultsSwap.plantsLong * resultsSwap.plantsWide + resultsSwap.crackFill)){
+            return resultsStd;
+        } else if ((resultsSwap.plantsLong * resultsSwap.plantsWide + resultsSwap.crackFill) > 
+          (resultsStd.plantsLong * resultsStd.plantsWide + resultsStd.crackFill)){
+            return resultsSwap;
         } else {
-            // Same as above, but for the "swap" naive packing solution
-            results.plantsLong = swapLCount;
-            results.plantsWide = swapWCount;
-
-            let leftoverCandidate1 = this.simpleMaxLayout(
-                (this.state.growSpaceWidthFt - (this.state.plantLengthFt * results.plantsWide)),
-                this.state.growSpaceLengthFt,
-                this.state.plantWidthFt,
-                this.state.plantLengthFt
-            );
-
-            let leftoverCandidate2 = this.simpleMaxLayout(
-                this.state.growSpaceWidthFt,
-                (this.state.growSpaceLengthFt - (this.state.plantWidthFt * results.plantsLong )),
-                this.state.plantWidthFt,
-                this.state.plantLengthFt
-            );
-
-            results.wastedSpacePlants = Math.max(leftoverCandidate1, leftoverCandidate2)
+            // prioritize minimum crackfilling if they're equal
+            if (resultsStd.crackFill < resultsSwap.crackFill){
+                return resultsStd;
+            } else {
+                return resultsSwap;
+            }
         }
-
-        return results;
     }
 
     getUsedSpace() {
         let layout = this.getPlantLayout();
-        return (layout.plantsLong * layout.plantsWide + layout.wastedSpacePlants) *
+        return (layout.plantsLong * layout.plantsWide + layout.crackFill) *
             (this.state.plantWidthFt * this.state.plantLengthFt);
     }
 
@@ -171,16 +199,6 @@ export default class PlantCount extends React.Component {
                     </div>
                     <div className="col-sm">
                         <FixedUnitOutput
-                            outputLabel="Total Area"
-                            number={this.state.growSpaceLengthFt * this.state.growSpaceWidthFt}
-                            unit="ft²"
-                        />
-                        <FixedUnitOutput
-                            outputLabel="Total Plants"
-                            number={this.getPlantLayout().plantsLong * this.getPlantLayout().plantsWide}
-                            unit="plants"
-                        />
-                        <FixedUnitOutput
                             outputLabel="Grid Layout"
                             number={this.getPlantLayout().plantsWide}
                             unit="plants wide by"
@@ -192,13 +210,29 @@ export default class PlantCount extends React.Component {
                         />
                         <FixedUnitOutput
                             outputLabel="with"
-                            number={this.getPlantLayout().wastedSpacePlants}
+                            number={this.getPlantLayout().crackFill}
                             unit="plants in remaining space"
+                        />
+                        <hr />
+                        <FixedUnitOutput
+                            outputLabel="Total Area"
+                            number={this.state.growSpaceLengthFt * this.state.growSpaceWidthFt}
+                            unit="ft²"
+                        />
+                        <FixedUnitOutput
+                            outputLabel="Total Plants"
+                            number={this.getPlantLayout().plantsLong * this.getPlantLayout().plantsWide}
+                            unit="plants"
                         />
                         <FixedUnitOutput
                             outputLabel="Wasted Space"
                             number={this.getWastedSpace()}
                             unit="ft²"
+                        />
+                        <FixedUnitOutput
+                            outputLabel="Space Efficiency"
+                            number={round(this.getUsedSpace() / (this.state.growSpaceLengthFt * this.state.growSpaceWidthFt) * 100, 2)}
+                            unit="%"
                         />
                     </div>
                 </div>
