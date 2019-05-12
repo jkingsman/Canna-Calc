104adfc4136b97f9c072ad8b51d5a0bb77246445
jkingsman <jack.kingsman@gmail.com>
2019-05-11 20:34:37 -0400

    Add extra space optimization to plant placement

--- src/calculators/plants_consumables/plantcount_calculator.jsx
+++ src/calculators/plants_consumables/plantcount_calculator.jsx
@@ -7,34 +7,128 @@ export default class PlantCount extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            growSpaceWidthFt: 17,
-            growSpaceLengthFt: 8,
+            growSpaceWidthFt: 10,
+            growSpaceLengthFt: 7,
             plantWidthFt: 3,
-            plantLengthFt: 2,
+            plantLengthFt: 1,
         };
     }
 
+    simpleMaxLayout(areaWidth, areaLength, objWidth, objLength) {
+        const stdWCount = Math.floor(areaWidth / objWidth);
+        const stdLCount = Math.floor(areaLength / objLength);
+
+        const swapWCount = Math.floor(areaWidth / objLength);
+        const swapLCount = Math.floor(areaLength / objWidth);
+
+        if (stdWCount * stdLCount > swapWCount * swapLCount) {
+            return stdWCount * stdLCount;
+        } else {
+            return swapWCount * swapLCount;
+        }
+    }
+
     getPlantLayout() {
+        /* Okay, this is a bit of a dumpster fire because
+         * I wrote it at 35kft and jetlagged BUT hopefully
+         * some liberal commenting will help you make some
+         * sense of this.
+         */
+
+         /* To start with, we do a naive block layout. 'std'
+          * is where grow space width value is aligned with
+          * plant width value and vice versa for length.
+          * That relationship is inverted for 'swap'.
+          * Basically, tile the plants one way, then the
+          * other way.
+          */
         const stdWCount = Math.floor(this.state.growSpaceWidthFt / this.state.plantWidthFt);
         const stdLCount = Math.floor(this.state.growSpaceLengthFt / this.state.plantLengthFt);
 
         const swapWCount = Math.floor(this.state.growSpaceWidthFt / this.state.plantLengthFt);
         const swapLCount = Math.floor(this.state.growSpaceLengthFt / this.state.plantWidthFt);
 
-        if (stdWCount * stdLCount > swapWCount * swapLCount) {
-            return [stdWCount, stdLCount];
-        } else {
-            return [swapLCount, swapWCount];
+        let results = {
+            plantsLong: 0,
+            plantsWide: 0,
+            wastedSpacePlants: 0,
+        };
+
+        /* If we can't permute either way (e.g. dimensions
+         * are too small),. return the default value set
+         * (zero everything)
+         */
+        if (stdWCount * stdLCount == 0 && swapWCount * swapLCount == 0) {
+            return results;
         }
+
+        /* Two big cases: out std layout was the superior
+         * naive layout, or the swap was.
+         */
+        if (stdWCount * stdLCount > swapWCount * swapLCount) {
+            results.plantsLong = stdLCount;
+            results.plantsWide = stdWCount;
+
+            /* After our naive layout, we have two cases for
+             * possible "fill-in-the-gaps" layout -- down
+             * one edge, or down the other.
+             *
+             * We do a simple "how many can we fit" check
+             * on both these options, then take the max.
+             */
+
+            let leftoverCandidate1 = this.simpleMaxLayout(
+                // Hint: The edge space's dimensions will be the leftover space * how long that edge is
+                //       Here, we check the "wide" leftover space.
+                (this.state.growSpaceWidthFt - (this.state.plantWidthFt * results.plantsWide)),
+                this.state.growSpaceLengthFt,
+                this.state.plantWidthFt,
+                this.state.plantLengthFt
+            );
+
+            let leftoverCandidate2 = this.simpleMaxLayout(
+                // ...and here the "long" leftover space
+                this.state.growSpaceWidthFt,
+                (this.state.growSpaceLengthFt - (this.state.plantLengthFt * results.plantsLong )),
+                this.state.plantWidthFt,
+                this.state.plantLengthFt
+            );
+
+            results.wastedSpacePlants = Math.max(leftoverCandidate1, leftoverCandidate2)
+        } else {
+            // Same as above, but for the "swap" naive packing solution
+            results.plantsLong = swapLCount;
+            results.plantsWide = swapWCount;
+
+            let leftoverCandidate1 = this.simpleMaxLayout(
+                (this.state.growSpaceWidthFt - (this.state.plantLengthFt * results.plantsWide)),
+                this.state.growSpaceLengthFt,
+                this.state.plantWidthFt,
+                this.state.plantLengthFt
+            );
+
+            let leftoverCandidate2 = this.simpleMaxLayout(
+                this.state.growSpaceWidthFt,
+                (this.state.growSpaceLengthFt - (this.state.plantWidthFt * results.plantsLong )),
+                this.state.plantWidthFt,
+                this.state.plantLengthFt
+            );
+
+            results.wastedSpacePlants = Math.max(leftoverCandidate1, leftoverCandidate2)
+        }
+
+        return results;
+    }
+
+    getUsedSpace() {
+        let layout = this.getPlantLayout();
+        return (layout.plantsLong * layout.plantsWide + layout.wastedSpacePlants) *
+            (this.state.plantWidthFt * this.state.plantLengthFt);
     }
 
     getWastedSpace() {
-        const usedSpace =
-            this.getPlantLayout()[0] *
-            this.getPlantLayout()[1] *
-            (this.state.plantWidthFt * this.state.plantLengthFt);
         const totalSpace = this.state.growSpaceLengthFt * this.state.growSpaceWidthFt;
-        return totalSpace - usedSpace;
+        return totalSpace - this.getUsedSpace();
     }
 
     render() {
@@ -68,13 +162,13 @@ export default class PlantCount extends React.Component {
                             inputLabel="Plant Width*"
                             onChange={val => this.setState({ plantLengthFt: Number(val) })}
                             conversionFactors={ConversionFactors.basicDistance}
-                            number={this.state.plantLengthFt}
+                            number={this.state.plantWidthFt}
                         />
                         <GenericInput
                             inputLabel="Plant Length*"
                             onChange={val => this.setState({ plantWidthFt: Number(val) })}
                             conversionFactors={ConversionFactors.basicDistance}
-                            number={this.state.plantWidthFt}
+                            number={this.state.plantLengthFt}
                         />
                     </div>
                     <div className="col-sm">
@@ -85,18 +179,23 @@ export default class PlantCount extends React.Component {
                         />
                         <FixedUnitOutput
                             outputLabel="Total Plants"
-                            number={this.getPlantLayout()[0] * this.getPlantLayout()[1]}
+                            number={this.getPlantLayout().plantsLong * this.getPlantLayout().plantsWide}
                             unit="plants"
                         />
                         <FixedUnitOutput
                             outputLabel="Grid Layout"
-                            number={this.getPlantLayout()[0]}
+                            number={this.getPlantLayout().plantsWide}
                             unit="plants wide by"
                         />
                         <FixedUnitOutput
                             noColon
-                            number={this.getPlantLayout()[1]}
+                            number={this.getPlantLayout().plantsLong}
                             unit="plants long"
+                        />
+                        <FixedUnitOutput
+                            outputLabel="with"
+                            number={this.getPlantLayout().wastedSpacePlants}
+                            unit="plants in remaining space"
                         />
                         <FixedUnitOutput
                             outputLabel="Wasted Space"
