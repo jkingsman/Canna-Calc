730d4eb33d3a03d345cb485a56c4822f5b6380c3
jkingsman <jack.kingsman@gmail.com>
2019-06-05 19:50:22 -0700

    Update test calc with dayjs stuff and emoji tests

--- src/calculators/general/testing.jsx
+++ src/calculators/general/testing.jsx
@@ -8,16 +8,17 @@ import {
 } from "app/calculators/components/io";
 import ConversionFactors from "app/utils/conversion_factors";
 import { defaultRound } from "app/utils/math";
+import dayjs from "dayjs";
 
 export default class TestingCalculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             abvIn: 40,
-            amtIn: 1,
+            amtIn: 3,
             numberConsumed: 1,
             weight: 230,
-            period: 45,
+            isMale: true,
         };
 
         this.alcoholPerDrink = {
@@ -32,20 +33,40 @@ export default class TestingCalculator extends React.Component {
     }
 
     _bacCalc(flOzEthanol, male, weightLbs, drinkingPeriodMins) {
-        let sd = flOzEthanol / 0.4826; // 10 grams ethanol = .4826 fl. oz
+        let sd = flOzEthanol * 23.342386982 / 10; // 1 fl oz ethanol = 23.3... grams; divide by ten for standard drinks
         let bw = male ? 0.58 : 0.49; // body water constant
         let kilos = weightLbs * 0.453592;
         let mr = male ? 0.015 : 0.017; // metabolism constant
         let dp = drinkingPeriodMins / 60;
 
-        return (0.806 * sd * 1.2 / (bw * kilos) - mr * dp) * 10;
+        return 0.806 * sd * 1.2 / (bw * kilos) - mr * dp;
     }
 
-    getBAC() {
-        return {
-            male: this._bacCalc(this.getEthanol(), true, this.state.weight, this.state.period),
-            female: this._bacCalc(this.getEthanol(), false, this.state.weight, this.state.period),
-        };
+    getBAC(drinkingPeriod) {
+        return Math.max(
+            this._bacCalc(this.getEthanol(), this.state.isMale, this.state.weight, drinkingPeriod),
+            0
+        );
+    }
+
+    getDriveStatusUnit(bac) {
+        let statusEmoji;
+
+        if (bac >= 0.08) {
+            statusEmoji = "💀";
+        } else if (bac <= 0.08 && bac >= 0.06) {
+            statusEmoji = "⚠️";
+        } else {
+            statusEmoji = "✅";
+        }
+
+        return `%  ${statusEmoji}`;
+    }
+
+    getMinsFromNow(mins) {
+        return dayjs()
+            .add(mins, "minute")
+            .format("HHmm");
     }
 
     render() {
@@ -72,6 +93,31 @@ export default class TestingCalculator extends React.Component {
                             number={this.state.numberConsumed}
                             unit="drinks"
                         />
+                        <div className="form-group">
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="male"
+                                    checked={this.state.isMale}
+                                    onChange={ev =>
+                                        this.setState({ isMale: ev.target.value === "male" })
+                                    }
+                                />
+                                Male
+                            </label>
+                            &nbsp;&nbsp;
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="female"
+                                    checked={!this.state.isMale}
+                                    onChange={ev =>
+                                        this.setState({ isMale: ev.target.value === "male" })
+                                    }
+                                />
+                                Female
+                            </label>
+                        </div>
                         <hr />
                         <h2>BAC Math</h2>
                         <FixedUnitInput
@@ -79,12 +125,6 @@ export default class TestingCalculator extends React.Component {
                             onChange={val => this.setState({ weight: Number(val) })}
                             number={this.state.weight}
                             unit="lbs"
-                        />
-                        <FixedUnitInput
-                            inputLabel="Drinking Period"
-                            onChange={val => this.setState({ period: Number(val) })}
-                            number={this.state.period}
-                            unit="minutes"
                         />
                     </div>
                     <div className="col-sm">
@@ -110,14 +150,39 @@ export default class TestingCalculator extends React.Component {
                             unit="wine bottles"
                         />
                         <FixedUnitOutput
-                            outputLabel="BAC (Male)"
-                            number={defaultRound(this.getBAC().male)}
-                            unit="%"
+                            outputLabel={`Instantaneous (${this.getMinsFromNow(0)})`}
+                            number={defaultRound(this.getBAC(0))}
+                            unit={this.getDriveStatusUnit(this.getBAC(0))}
                         />
                         <FixedUnitOutput
-                            outputLabel="BAC (Female)"
-                            number={defaultRound(this.getBAC().female)}
-                            unit="%"
+                            outputLabel={`+30 minutes (${this.getMinsFromNow(30)})`}
+                            number={defaultRound(this.getBAC(30))}
+                            unit={this.getDriveStatusUnit(this.getBAC(30))}
+                        />
+                        <FixedUnitOutput
+                            outputLabel={`+1.0 hour (${this.getMinsFromNow(60)})`}
+                            number={defaultRound(this.getBAC(60))}
+                            unit={this.getDriveStatusUnit(this.getBAC(60))}
+                        />
+                        <FixedUnitOutput
+                            outputLabel={`+1.5 hour (${this.getMinsFromNow(90)})`}
+                            number={defaultRound(this.getBAC(90))}
+                            unit={this.getDriveStatusUnit(this.getBAC(90))}
+                        />
+                        <FixedUnitOutput
+                            outputLabel={`+2.0 hour (${this.getMinsFromNow(120)})`}
+                            number={defaultRound(this.getBAC(120))}
+                            unit={this.getDriveStatusUnit(this.getBAC(120))}
+                        />
+                        <FixedUnitOutput
+                            outputLabel={`+2.5 hour (${this.getMinsFromNow(150)})`}
+                            number={defaultRound(this.getBAC(150))}
+                            unit={this.getDriveStatusUnit(this.getBAC(150))}
+                        />
+                        <FixedUnitOutput
+                            outputLabel={`+3.0 hour (${this.getMinsFromNow(180)})`}
+                            number={defaultRound(this.getBAC(180))}
+                            unit={this.getDriveStatusUnit(this.getBAC(180))}
                         />
                     </div>
                 </div>
