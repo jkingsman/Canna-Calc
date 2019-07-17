60a8ea63e65a8f2e29e4aac26b6603b896419f65
jkingsman <jack.kingsman@gmail.com>
2019-07-16 17:22:52 -0700

    No bad style flash on load

--- src/calculators/calculator_container.jsx
+++ src/calculators/calculator_container.jsx
@@ -111,6 +111,7 @@ export default class CalculatorContainer extends React.Component {
                 }, 250);
             }, 100);
         }
+        document.body.style.display = "block"
     }
 
     renderBAC() {
